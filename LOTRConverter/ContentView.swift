9722d5556f20a184a9881ba1a6344d39d1453750
joaobug <joaoenfrance@gmail.com>
2024-08-23 15:40:51 -0300

    redoing lOTR converter for practice purposes
    -started doing the UI using ZStack to put the background and some v and h stacks for the text and texfields.
    - created a new view called ExchangeInfo that will be shown when user clicks the info button

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -8,15 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rightAmount: String = "fff"
+    @State private var leftAmount: String = "fff"
+    @State private var showingExchangeInfo = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+                .opacity(0.9)
+            VStack{
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    
+                Text("Currency Exchange")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                
+                HStack{
+                    VStack{
+                        HStack{
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                        }
+                        TextField("Right", text: $rightAmount)
+                        
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    VStack{
+                        HStack{
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        TextField("Crre", text: $rightAmount)
+                            .textFieldStyle(.roundedBorder)
+                        
+                    }
+                }
+                Spacer()
+                HStack{
+                    Spacer()
+                    Button {
+                        showingExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                    .sheet(isPresented: $showingExchangeInfo) {
+                        ExchangeInfo()
+                    }
+                }
+                
+                
+
+                
+            }
+            
+            
         }
-        .padding()
+
     }
+  
 }
 
 #Preview {
---
+++ LOTRConverter/Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  LOTRConverter
+//
+//  Created by Joao Leal on 8/23/24.
+//
+
